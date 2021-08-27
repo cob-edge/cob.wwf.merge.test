b1030e1cdd5d79a8ed35547baf2a329628add3b9
Jarvi1 <jarvis.cole1@gmail.com>
2021-08-28 09:56:36 +1000

    sql api updates

--- Controllers/IoTController.cs
+++ Controllers/IoTController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Harley.UAT.Controllers
 {
@@ -22,7 +23,10 @@ namespace Harley.UAT.Controllers
         [HttpGet]
         public IEnumerable<IoT> Get()
         {
-            //var rng = new Random();
+            //onnect();
+            //Read();
+
+            //var rng = new Random(); //range is how many times they will appears
             return Enumerable.Range(1, 5).Select(index => new IoT
             {
                 SensorId = 1,
@@ -33,41 +37,31 @@ namespace Harley.UAT.Controllers
                 V2 = 2.22, 
                 V3 = 3.33, 
                 Latitude = 6.66, 
-                Longitude = 9.99
+                Longitude = 9.99 
             })
             .ToArray();
         }
 
         private static SqlConnection sqlc;
-
-        public class SQLWraper
+        public void Connect()
         {
-            public string connectionString { set; get; }
-            //private SqlConnection sqlc;
-            public SQLWraper()
+            String connectionString = "Server=tcp:cob-edge.database.windows.net,1433;Initial Catalog=IoTDB;Persist Security Info=False;User ID=cob.edge.admin;Password=Aoed7Test;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            try
             {
-                connectionString = "Server=tcp:cob-edge.database.windows.net,1433;Initial Catalog=IoTDB;Persist Security Info=False;User ID=cob.edge.admin;Password=Aoed7Test;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                Connect();
+                if (!String.IsNullOrEmpty(connectionString))
+                {
+                    sqlc = new SqlConnection(connectionString);
+                    Console.WriteLine("Connected to SQL Server");
+                }
             }
-            public void Connect()
+            catch (Exception e2)
             {
-                try
-                {
-                    if (!String.IsNullOrEmpty(connectionString))
-                    {
-                        sqlc = new SqlConnection(connectionString);
-                        Console.WriteLine("Connected to SQL Server");
-                    }
-                }
-                catch (Exception e2)
-                {
-                    Console.WriteLine("SQL Connection Exception: " + e2.Message);
-                }
+                Console.WriteLine("SQL Connection Exception: " + e2.Message);
             }
         }
 
-        /*
-        public void Read()
+        private IoT[] IoTs;
+        public void Read() //coould me modified for specific queires, then just retreive whole table
         {
             //Read DB table 
             SqlCommand cmd = new SqlCommand(@"
@@ -78,9 +72,27 @@ namespace Harley.UAT.Controllers
             sqlc.Open();
             SqlDataReader reader = cmd.ExecuteReader();
             Results.Load(reader);
+            int SizeIoTs = Results.Rows.Count;
             sqlc.Close();
+
+            //Set database objects into a array, that can then be passed to webpage
+            IoTs = new IoT[SizeIoTs];
+            int i = 0;
             foreach (DataRow row in Results.Rows)
             {
+                IoTs[i] = new IoT {
+                    SensorId = (int)row["SensorId"],
+                    TimeStamp = DateTime.Now /* row["TimeStamp"].ToString() */,
+                    Description = row["Description"].ToString(),
+                    Type = row["Type"].ToString(),
+                    V1 = (int)row["V1"],
+                    V2 = (int)row["V2"],
+                    V3 = (int)row["V3"],
+                    Latitude = Convert.ToSingle(row["Latitude"]),
+                    Longitude = Convert.ToSingle(row["Longitude"])
+                };
+                i++;
+                /*
                 JsonMsg jsonMsg = new JsonMsg();
                 jsonMsg.id = (int)row["SensorId"];
                 jsonMsg.Timestamp = row["TimeStamp"].ToString();
@@ -92,8 +104,9 @@ namespace Harley.UAT.Controllers
                 jsonMsg.Latitude = Convert.ToSingle(row["Latitude"]);
                 jsonMsg.Longitude = Convert.ToSingle(row["Longitude"]);
                 Console.WriteLine(jsonMsg.GetLogString());
+                */
             }
         }
-        */
+        
     }
 }
