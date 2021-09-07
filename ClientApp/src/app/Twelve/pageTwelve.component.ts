32ff65dc7bd6f0991203efabb0233a18699a87d7
MarcusDelios <20348449@students.latrobe.edu.au>
2021-09-07 13:52:01 +1000

    customer data + pw javascript

---
+++ ClientApp/src/app/Twelve/pageTwelve.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Twelve',
+  templateUrl: './pageTwelve.component.html',
+  styleUrls: ['../../assets/css/main.css']
+})
+export class Twelve {
+}
+
