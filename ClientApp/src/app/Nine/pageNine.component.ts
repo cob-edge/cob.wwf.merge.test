beeba726a38f699143961d98c879e4265dfc3700
Harley_19957195 <19957195@students.latrobe.edu.au>
2021-09-11 18:42:26 +1000

    Working DB tables on Page 4 & Page 9, added User controller and API

--- ClientApp/src/app/Nine/pageNine.component.ts
+++ ClientApp/src/app/Nine/pageNine.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-Nine',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['../../assets/css/main.css']
 })
 export class Nine {
+  public users: User[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<User[]>(baseUrl + 'user').subscribe(result => {
+      this.users = result;
+    }, error => console.error(error));
+  }
+}
+
+interface User {
+  user_ID: number;
+  user_FirstName: string;
+  user_LastName: string;
+  userType: string;
+  user_Email: string;
 }
 
