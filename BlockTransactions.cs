734c311df2c030f9022cc772417677ca29ff7ea9
Jarvis <jarvis.cole1@gmail.com>
2021-09-11 16:43:16 +1000

    new api's transactions, user, recent v1 v2 v3

---
+++ BlockTransactions.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Harley.UAT
+{
+    public class BlockTransactions 
+    {
+        public string Hash { get; set; }
+
+        public int Nonce { get; set; }
+
+        public string Block_Hash { get; set; }
+
+        public int Block_Number { get; set; }
+
+        public int Transaction_Index { get; set; }
+
+        public string From_Address { get; set; }
+
+        public string To_Address { get; set; }
+
+        public int Value { get; set; }
+
+        public int Gas { get; set; }
+
+        public float Gas_Price { get; set; }
+
+        public int Timestamp { get; set; }
+        
+        public int Max_Fee_Per_Gas { get; set; }
+
+        public int Max_Priority_Fee_Per_Gas { get; set; }
+
+        public int Transaction_Type { get; set; }
+
+        public int Sensor_ID { get; set; }
+    }
+
+    public class RecentGas
+    {
+        public double[] Recent10 { get; set; }
+    }
+}
---
+++ Controllers/RecentV2Controller.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Harley.UAT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecentV2Controller : ControllerBase
+    {
+        private readonly ILogger<RecentV2Controller> _logger;
+
+        public RecentV2Controller(ILogger<RecentV2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<RecentV2> Get()
+        {
+            Connect();
+            //Read();
+            return Enumerable.Range(1, 1).Select(index => new RecentV2
+            {
+                Recent10 = getRecent10()
+            })
+            .ToArray();
+        }
+
+        public double[] getRecent10()
+        {
+            Read();
+            return recentV2.Recent10;
+        }
+
+        private static SqlConnection sqlc;
+        public void Connect()
+        {
+            String connectionString = "Server=tcp:cob-edge.database.windows.net,1433;Initial Catalog=IoTDB;Persist Security Info=False;User ID=cob.edge.admin;Password=Aoed7Test;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            try
+            {
+                if (!String.IsNullOrEmpty(connectionString))
+                {
+                    sqlc = new SqlConnection(connectionString);
+                    Console.WriteLine("Connected to SQL Server");
+                }
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine("SQL Connection Exception: " + e2.Message);
+            }
+        }
+
+        private RecentV2 recentV2;
+        public void Read() //could me modified for specific queires, then just retreive whole table
+        {
+            //Read DB table 
+            SqlCommand cmd = new SqlCommand(@"
+               SELECT TOP (10) [SensorId], [V2], [TimeStamp] 
+                  FROM [dbo].[IOT]
+	              WHERE [Type]='Vehicle' AND [SensorId]=66
+	              ORDER BY [TimeStamp] DESC
+               ", sqlc);
+            DataTable Results = new DataTable();
+            // Read table from database and store it
+            sqlc.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Results.Load(reader);
+            int SizeRecent10 = Results.Rows.Count;
+            sqlc.Close();
+
+            //Set database objects into a array, that can then be passed to webpage
+            recentV2 = new RecentV2();
+            recentV2.Recent10 = new double[SizeRecent10];
+            int i = 0;
+            foreach (DataRow row in Results.Rows)
+            {
+                recentV2.Recent10[i] = (int)row["V2"];
+                i++;
+            }
+        }
+    }
+}
