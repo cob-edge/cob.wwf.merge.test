9075bbff9643c19ba3ca1ad80302fc4f7a39144d
Harley_19957195 <19957195@students.latrobe.edu.au>
2021-09-14 17:22:34 +1000

    Implemented new form on page 3 along with Test object and services typescript to facilitate data moving from website to DB, still working on

--- ClientApp/src/app/Three/pageThree.component.ts
+++ ClientApp/src/app/Three/pageThree.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'app-Three',
@@ -7,24 +7,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['../../assets/css/main.css']
 })
 export class Three {
-  public iots: IoT[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<IoT[]>(baseUrl + 'iot').subscribe(result => {
-      this.iots = result;
-    }, error => console.error(error));
+  constructor(private _testService: TestService) { }
+
+  onSubmit() {
+    console.log("Form submitted successfully")
   }
 }
-
-interface IoT {
-  sensorId: number;
-  timeStamp: string;
-  description: string;
-  type: string;
-  v1: number;
-  v2: number;
-  v3: number;
-  latitude: number;
-  longitude: number;
-}
-
---
+++ Test.cs
@@ -0,0 +1,13 @@
+namespace Harley.UAT
+{
+    public class Test
+    {
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public int Phone { get; set; }
+    }
+}
