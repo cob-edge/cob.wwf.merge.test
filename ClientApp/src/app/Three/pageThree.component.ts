75382e85ea40afb72d64bbef4af7d3639c7f59a3
Jarvis <jarvis.cole1@gmail.com>
2021-09-18 18:32:26 +1000

    home page updates, bigger more links

--- ClientApp/src/app/Three/pageThree.component.ts
+++ ClientApp/src/app/Three/pageThree.component.ts
@@ -1,30 +1,142 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Chart } from 'chart.js';
 import { HttpClient } from '@angular/common/http';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
-  selector: 'app-Three',
+  selector: 'app-three',
   templateUrl: './pageThree.component.html',
   styleUrls: ['../../assets/css/main.css']
 })
-export class Three {
-  public iots: IoT[];
 
+export class Three implements OnInit {
+  //live data declaration
+  private updateSubscription: Subscription;
+
+  //chart js declaration
+  title = 'livechart';
+  chart;
+
+  title2 = 'livechart';
+  chart2;
+
+  //api declaration
+  public recentV1s: RecentV1[];
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<IoT[]>(baseUrl + 'iot').subscribe(result => {
-      this.iots = result;
+    http.get<RecentV1[]>(baseUrl + 'recentV1').subscribe(result => {
+      this.recentV1s = result;
+      this.recentV2s = result;
+      this.http = http;
+      this.baseUrl = baseUrl;
     }, error => console.error(error));
+  }
+
+  //api updates from database
+  public http: HttpClient;
+  public baseUrl: string;
+  updateApiCall(http: HttpClient, baseUrl: string) {
+    http.get<RecentV1[]>(baseUrl + 'recentV1').subscribe(result => {
+      this.recentV1s = result;
+    }, error => console.error(error));
+  }
+
+  //other api updates from database
+  public recentV2s: RecentV2[];
+  updateApiCall2(http: HttpClient, baseUrl: string) {
+    http.get<RecentV2[]>(baseUrl + 'recentV2').subscribe(result => {
+      this.recentV2s = result;
+    }, error => console.error(error));
+  }
+
+  //run
+  ngOnInit() {
+    this.createTestChart();
+    this.createTestChart2();
+
+    this.updateSubscription = interval(3000).subscribe(
+      (val) => { this.updateStats() });
+  }
+
+  createTestChart() {
+    //chart creation
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Realtime Charts'
+        },
+        scales: {
+
+        }
+      },
+      data: {
+        labels: ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Live Speed detected by sensor',
+            //data: [10, 3, 6, 11, 38, 5, 6, 17],
+            backgroundColor: '#3F3FBF',
+            fill: false
+          }
+        ]
+      }
+    });
+  }
+
+  createTestChart2() {
+    //chart creation
+    this.chart2 = new Chart('canvas2', {
+      type: 'line',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Realtime Charts'
+        },
+        scales: {
+
+        }
+      },
+      data: {
+        labels: ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Live Speed detected by sensor',
+            data: [1, 1, 1, 1, 1, 1, 1, 1],
+            backgroundColor: '#3F3FBF',
+            fill: false
+          }
+        ]
+      }
+    });
+  }
+
+  updateStats() { //this method here does the live data refresh
+    this.updateApiCall(this.http, this.baseUrl); //re runs the sql query to get 10 most recent v1 values
+    this.updateApiCall2(this.http, this.baseUrl);
+
+    console.log("hello from update status chart : " + this.recentV1s[0].recent10);
+    this.chart.data.datasets[0].data = this.recentV1s[0].recent10;
+    this.chart.update();
+
+    console.log("hello from update status chart2 : " + this.recentV2s[0].recent10);
+    this.chart2.data.datasets[0].data = this.recentV2s[0].recent10;
+    this.chart2.update();
   }
 }
 
-interface IoT {
-  sensorId: number;
-  timeStamp: string;
-  description: string;
-  type: string;
-  v1: number;
-  v2: number;
-  v3: number;
-  latitude: number;
-  longitude: number;
+interface RecentV1 {
+  recent10: number[];
 }
+
+interface RecentV2 {
+  recent10: number[];
+}
+
+
+
 
