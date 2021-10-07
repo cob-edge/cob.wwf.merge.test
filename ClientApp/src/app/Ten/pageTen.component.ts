a817eacad4355e6c09f9e0ea72afab3fb600cd9f
Jarvis <jarvis.cole1@gmail.com>
2021-10-07 17:39:04 +1100

    Front End Clean ups pages eight, nine, api login button

--- ClientApp/src/app/Ten/pageTen.component.ts
+++ ClientApp/src/app/Ten/pageTen.component.ts
@@ -11,132 +11,80 @@ export class Ten {
 
   //constructor(private service: SharedService) { }
 
-  postData = {
-    test: 'my content yoo'
-  };
-
-  public users: User[];
-  /*
-  constructor(private http: HttpClient) {
-    this.http.post('https://httpbin.org/post', this.postData).toPromise().then(data => {
-      console.log(data);
-    })
-  }
-  */
-
   
   //public recentV1s: RecentV1[];
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    this.user = {
-      /*
-      "user_ID": 301,
-      "user_FirstName": "Hayley",
-      "user_LastName": "Mcmahon",
-      "user_Type": "M",
-      "user_Email": "natoque.penatibus@odiosagittis.org",
-      "user_PhoneNo": "0486 312 519        ",
-      "user_Address_Street": "974-1626 Elementum Rd.",
-      "user_Address_City": "Lewiston",
-      "user_Address_Postcode": 3837,
-      "user_LicenseNo": "533401519-00004     ",
-      "user_LicenseExp": "9/12/2024 12:00:00 AM"
-      */
 
-      user_ID: 301,
-      user_FirstName: "Hayley",
-      user_LastName: "Mcmahon",
-      user_Type: "M",
-      user_Email: "natoque.penatibus@odiosagittis.org",
-      user_PhoneNo: "0486 312 519        ",
-      user_Address_Street: "974-1626 Elementum Rd.",
-      user_Address_City: "Lewiston",
-      user_Address_Postcode: 3837,
-      user_LicenseNo: "533401519-00004     ",
-      user_LicenseExp: "9/12/2024 12:00:00 AM"
-      
-    }
-
-    console.log(this.user);
-
-    //this.users[0] = this.user
-
-    //console.log(this.users[0]);
-
-    this.http.post<User>(baseUrl + 'user', this.user).subscribe(res => {
-      alert(res.toString());
-      console.log(res.toString());
-    })
-
-    
-
+    this.baseUrl = baseUrl;    
   }
+  baseUrl: string
   
 
   @Input() user: any;
-  User_ID: number;
-  User_FirstName: string;
-  User_LastName: string;
-  User_Type: string;
-  User_Email: string;
-  User_Password: string;
-  User_PhoneNo: string;
-  User_Address_Street: string;
-  User_Address_City: string;
-  User_Address_Postcode: number;
-  User_LicenseNo: string;
-  User_LicenseExp: string;
+  user_ID: number;
+  user_FirstName: string;
+  user_LastName: string;
+  user_Type: string;
+  user_Email: string;
+  user_Password: string;
+  user_PhoneNo: string;
+  user_Address_Street: string;
+  user_Address_City: string;
+  user_Address_Postcode: number;
+  user_LicenseNo: string;
+  user_LicenseExp: string;
 
   ngOnInit(): void {
     this.user = {
-      User_ID: -1,
-      User_FirstName: "",
-      User_LastName: "",
-      User_Type: "",
-      User_Email: "",
-      User_Password: "",
-      User_PhoneNo: "",
-      User_Address_Street: "",
-      User_Address_City: "",
-      User_Address_Postcode: "",
-      User_LicenseNo: "",
-      User_LicenseExp: ""
+      user_ID: -1,
+      user_FirstName: "",
+      user_LastName: "",
+      user_Type: "",
+      user_Email: "",
+      user_Password: "",
+      user_PhoneNo: "",
+      user_Address_Street: "",
+      user_Address_City: "",
+      user_Address_Postcode: "",
+      user_LicenseNo: "",
+      user_LicenseExp: ""
     }
 
-    this.User_ID = this.user.User_ID; 
-    this.User_FirstName = this.user.User_FirstName;
-    this.User_LastName = this.user.User_LastName;
-    this.User_Type = this.user.User_Type;
-    this.User_Email = this.user.User_Email;
-    this.User_Password = this.user.User_Password;
-    this.User_PhoneNo = this.user.User_PhoneNo;
-    this.User_Address_Street = this.user.User_Address_Street;
-    this.User_Address_City = this.user.User_Address_City;
-    this.User_Address_Postcode = this.user.User_Address_Postcode;
-    this.User_LicenseNo = this.user.User_LicenseNo;
-    this.User_LicenseExp = this.user.User_LicenseExp;
+    this.user_ID = this.user.User_ID; 
+    this.user_FirstName = this.user.User_FirstName;
+    this.user_LastName = this.user.User_LastName;
+    this.user_Type = this.user.User_Type;
+    this.user_Email = this.user.User_Email;
+    this.user_Password = this.user.User_Password;
+    this.user_PhoneNo = this.user.User_PhoneNo;
+    this.user_Address_Street = this.user.User_Address_Street;
+    this.user_Address_City = this.user.User_Address_City;
+    this.user_Address_Postcode = this.user.User_Address_Postcode;
+    this.user_LicenseNo = this.user.User_LicenseNo;
+    this.user_LicenseExp = this.user.User_LicenseExp;
   }
 
   checkUser() {
     var val = {
-      User_ID: this.User_ID, //dummy and defaults exist
-      User_FirstName: this.User_FirstName,
-      User_LastName: this.User_LastName,
-      User_Type: this.User_Type,
-      User_Email: this.User_Email, //in html 
-      User_Password: this.User_Password, //in html
-      User_PhoneNo: this.User_PhoneNo,
-      User_Address_Street: this.User_Address_Street,
-      User_Address_City: this.User_Address_City,
-      User_Address_Postcode: this.User_Address_Postcode,
-      User_LicenseNo: this.User_LicenseNo,
-      User_LicenseExp: this.User_LicenseExp};
-    console.log("User Email: " + this.User_Email);
-    console.log("User Password: " + val.User_Password);
-    //console.log("User Postcode: " + val.User_Address_Postcode);
+      user_ID: this.user_ID, //dummy and defaults exist
+      user_FirstName: this.user_FirstName,
+      user_LastName: this.user_LastName,
+      user_Type: this.user_Type,
+      user_Email: this.user_Email, //in html 
+      user_Password: this.user_Password, //in html
+      user_PhoneNo: this.user_PhoneNo,
+      user_Address_Street: this.user_Address_Street,
+      user_Address_City: this.user_Address_City,
+      user_Address_Postcode: this.user_Address_Postcode,
+      user_LicenseNo: this.user_LicenseNo,
+      user_LicenseExp: this.user_LicenseExp};
+    console.log("User Email: " + this.user_Email);
+    console.log("User Password: " + val.user_Password);
 
-    /*this.service.checkUser(val).subscribe(res => {
+    this.http.post<User>(this.baseUrl + 'user', val).subscribe(res => {
       alert(res.toString());
-    });*/
+      console.log(res.toString());
+    });
   }
 }
 
