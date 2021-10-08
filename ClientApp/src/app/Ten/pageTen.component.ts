2cdf0ef8e86e9960a3a820a96eda637ff0860774
Jarvis <jarvis.cole1@gmail.com>
2021-10-08 13:07:34 +1100

    Login Updates

--- ClientApp/src/app/Ten/pageTen.component.ts
+++ ClientApp/src/app/Ten/pageTen.component.ts
@@ -13,89 +13,39 @@ export class Ten {
 
   //public recentV1s: RecentV1[];
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    this.baseUrl = baseUrl;    
+    this.baseUrl = baseUrl;
   }
   baseUrl: string
 
-  @Input() user: any;
-  user_ID: number;
-  user_FirstName: string;
-  user_LastName: string;
-  user_Type: string;
-  user_Email: string;
-  user_Password: string;
-  user_PhoneNo: string;
-  user_Address_Street: string;
-  user_Address_City: string;
-  user_Address_Postcode: number;
-  user_LicenseNo: string;
-  user_LicenseExp: string;
+  @Input() login: any;
+  login_Email_Input: string;
+  login_Password_Input: string;
 
   ngOnInit(): void {
-    this.user = {
-      user_ID: -1,
-      user_FirstName: "",
-      user_LastName: "",
-      user_Type: "",
-      user_Email: "",
-      user_Password: "",
-      user_PhoneNo: "",
-      user_Address_Street: "",
-      user_Address_City: "",
-      user_Address_Postcode: "",
-      user_LicenseNo: "",
-      user_LicenseExp: ""
+    this.login = {
+      login_Email_Input: "",
+      login_Password_Input: ""
     }
 
-    this.user_ID = this.user.User_ID; 
-    this.user_FirstName = this.user.User_FirstName;
-    this.user_LastName = this.user.User_LastName;
-    this.user_Type = this.user.User_Type;
-    this.user_Email = this.user.User_Email;
-    this.user_Password = this.user.User_Password;
-    this.user_PhoneNo = this.user.User_PhoneNo;
-    this.user_Address_Street = this.user.User_Address_Street;
-    this.user_Address_City = this.user.User_Address_City;
-    this.user_Address_Postcode = this.user.User_Address_Postcode;
-    this.user_LicenseNo = this.user.User_LicenseNo;
-    this.user_LicenseExp = this.user.User_LicenseExp;
+    this.login_Email_Input = this.login.login_Email_Input;
+    this.login_Password_Input = this.login.login_Password_Input;
   }
 
-  checkUser() {
-    var val = {
-      user_ID: /*this.user_ID*/ 401, //dummy and defaults exist
-      user_FirstName: /*this.user_FirstName*/ "TestJarvis",
-      user_LastName: /*this.user_LastName*/ "TestCole",
-      user_Type: /*this.user_Type*/ "A",
-      user_Email: this.user_Email, //in html 
-      user_Password: this.user_Password, //in html
-      user_PhoneNo: this.user_PhoneNo,
-      user_Address_Street: this.user_Address_Street,
-      user_Address_City: this.user_Address_City,
-      user_Address_Postcode: this.user_Address_Postcode,
-      user_LicenseNo: this.user_LicenseNo,
-      user_LicenseExp: this.user_LicenseExp};
-    console.log("User Email: " + this.user_Email);
-    console.log("User Password: " + val.user_Password);
+  checkLogin() {
+    var login = {
+      login_Email_Input: this.login_Email_Input, //in html
+      login_Password_Input: this.login_Password_Input, //in html
 
-    this.http.post<User>(this.baseUrl + 'user', val).subscribe(res => {
+    }
+    this.http.post<Login>(this.baseUrl + 'login', login).subscribe(res => {
       alert(res.toString());
       console.log(res.toString());
     });
-  }
+  } 
 }
 
-interface User {
-  User_ID: number;
-  User_FirstName: string;
-  User_LastName: string;
-  User_Type: string;
-  User_Email: string;
-  User_Password: string;
-  User_PhoneNo: string;
-  User_Address_Street: string;
-  User_Address_City: string;
-  User_Address_Postcode: number;
-  User_LicenseNo: string;
-  User_LicenseExp: string;
+interface Login {
+  Login_Email_Input: string;
+  Login_Password_Input: string;
 }
+
