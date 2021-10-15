23763e5fb1860e5ad01ba217bc3b295c3e32f829
Jarvis <jarvis.cole1@gmail.com>
2021-10-16 00:20:17 +1100

    API updates more

--- ClientApp/src/app/Eighteen/pageEighteen.component.ts
+++ ClientApp/src/app/Eighteen/pageEighteen.component.ts
@@ -14,118 +14,25 @@ export class Eighteen implements OnInit {
   private updateSubscription: Subscription;
 
   //chart js declaration
-  title = 'livechart';
-  chart;
-
-  title2 = 'livechart';
-  chart2;
-
   title3 = 'livechart';
   chart3;
 
   //api declaration
-  public recentV1s: RecentV1[];
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<RecentV1[]>(baseUrl + 'recentV1').subscribe(result => {
-      this.recentV1s = result;
-      this.recentV2s = result;
-      this.recentV3s = result;
       this.http = http;
       this.baseUrl = baseUrl;
-    }, error => console.error(error));
   }
 
   //api updates from database V1
   public http: HttpClient;
   public baseUrl: string;
-  updateApiCall(http: HttpClient, baseUrl: string) {
-    http.get<RecentV1[]>(baseUrl + 'recentV1').subscribe(result => {
-      this.recentV1s = result;
-    }, error => console.error(error));
-  }
-
-  //other api updates from database V2
-  public recentV2s: RecentV2[];
-  updateApiCall2(http: HttpClient, baseUrl: string) {
-    http.get<RecentV2[]>(baseUrl + 'recentV2').subscribe(result => {
-      this.recentV2s = result;
-    }, error => console.error(error));
-  }
-
-  //other api updates from database V3
-  public recentV3s: RecentV2[];
-  updateApiCall3(http: HttpClient, baseUrl: string) {
-    http.get<RecentV3[]>(baseUrl + 'recentV3').subscribe(result => {
-      this.recentV3s = result;
-    }, error => console.error(error));
-  }
 
   //run
   ngOnInit() {
-    //this.createTestChart();
-    //this.createTestChart2();
     this.createTestChart3();
 
     this.updateSubscription = interval(3000).subscribe(
       (val) => { this.updateStats() });
-  }
-
-  createTestChart() {
-    //chart creation
-    this.chart = new Chart('canvas', {
-      type: 'bar',
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'My Ethereum Costs (AUD) of Past Day'
-        },
-        scales: {
-
-        }
-      },
-      data: {
-        labels: ['t-9', 't-8', 't-7', 't-6', 't-5', 't-4', 't-3', 't-2', 't-1', 't'],
-        datasets: [
-          {
-            type: 'bar',
-            label: 'Costs (AUD) of Past Day',
-            data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            backgroundColor: '#3F3FBF',
-            fill: false
-          }
-        ]
-      }
-    });
-  }
-
-  createTestChart2() {
-    //chart creation
-    this.chart2 = new Chart('canvas2', {
-      type: 'bar',
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Equivalent CarPark Costs of Past Day'
-        },
-        scales: {
-
-        }
-      },
-      data: {
-        labels: ['t-9', 't-8', 't-7', 't-6', 't-5', 't-4', 't-3', 't-2', 't-1', 't'],
-        datasets: [
-          {
-            type: 'bar',
-            label: 'Costs (AUD) of Past Day',
-            data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            backgroundColor: '#3F3FBF',
-            fill: false
-          }
-        ]
-      }
-    });
   }
 
   createTestChart3() {
@@ -151,7 +58,7 @@ export class Eighteen implements OnInit {
       'Wilson Parking'
     ],
     datasets: [{
-      label: 'Total Cars Currently Parked Across Network',
+      label: 'Total Cars Currently Parked Across Network As a Percentage',
       data: [1, 1, 1, 1],
       fill: true,
       backgroundColor: 'rgb(63,63,191, 0.2)',
@@ -163,37 +70,52 @@ export class Eighteen implements OnInit {
     }]
   };
 
+  User_ID: number;
   updateStats() { //this method here does the live data refresh
-    //this.updateApiCall(this.http, this.baseUrl); //re runs the sql query to get 10 most recent v1 values
-    //this.updateApiCall2(this.http, this.baseUrl);
-    //this.updateApiCall3(this.http, this.baseUrl);
-
-    //console.log("hello from update status chart : " + this.recentV1s[0].recent10);
-    //this.chart.data.datasets[0].data = [1, 0.5, 1, 2, 1, 3, 1, 4, 2, 1];
-    //this.chart.update();
-
-    //console.log("hello from update status chart2 : " + this.recentV2s[0].recent10);
-    //this.chart2.data.datasets[0].data = [2, 9, 3, 5, 9, 7, 13, 20, 12, 10];
-    //this.chart2.update();
-
-    //console.log("hello from update status chart2 : " + this.recentV2s[0].recent10);
-    this.chart3.data.datasets[0].data = [63, 20, 108, 27];
+    this.getChartDataChart3()
     this.chart3.update();
+  }
+
+  getChartDataChart3() {
+    this.http.get<RecentCarParkFill>(this.baseUrl + 'recentCarParkFill').subscribe(result => {
+      this.chart3.data.datasets[0].data = result.recent4;
+    }, error => console.error(error));
+  }
+
+  ipAddress: string;
+  async getIPAddress() {
+    this.http.get<{ ip: string }>('https://jsonip.com')
+      .subscribe(data => {
+        this.ipAddress = data.ip;
+      })
+  }
+
+  getUserData() {
+    this.http.get<User>(this.baseUrl + 'user/' + this.ipAddress).subscribe(result => {
+      this.User_ID = result.User_ID;
+    }, error => console.error(error));
   }
 }
 
-interface RecentV1 {
-  recent10: number[];
+interface RecentCarParkFill {
+  recent4: number[];
 }
 
-interface RecentV2 {
-  recent10: number[];
+interface User {
+  User_ID: number;
+  User_FirstName: string;
+  User_LastName: string;
+  User_Type: string;
+  User_Email: string;
+  User_Password: string;
+  User_PhoneNo: string;
+  User_Address_Street: string;
+  User_Address_City: string;
+  User_Address_Postcode: number;
+  User_LicenseNo: string;
+  User_LicenseExp: string;
+  User_IP_Address: string;
 }
-
-interface RecentV3 {
-  recent10: number[];
-}
-
 
 
 
---
+++ Controllers/RecentCarParkFillController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Harley.UAT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecentCarParkFillController : ControllerBase
+    {
+        private readonly ILogger<RecentCarParkFillController> _logger;
+
+        public RecentCarParkFillController(ILogger<RecentCarParkFillController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET METHOD FOR PAGE ELEVEN CHART 1 CHART 2 
+        [HttpGet]
+        public RecentCarParkFill Get()
+        {
+            Connect();
+            Read();
+            return recentCarParkFill; 
+        }
+
+        //connect to database object 
+        private static SqlConnection sqlc;
+        public void Connect()
+        {
+            String connectionString = "Server=tcp:cob-edge.database.windows.net,1433;Initial Catalog=IoTDB;Persist Security Info=False;User ID=cob.edge.admin;Password=Aoed7Test;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            try
+            {
+                if (!String.IsNullOrEmpty(connectionString))
+                {
+                    sqlc = new SqlConnection(connectionString);
+                    Console.WriteLine("Connected to SQL Server");
+                }
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine("SQL Connection Exception: " + e2.Message);
+            }
+        }
+
+        //private static double WeiToAudRatio = 0.0000000000000045;
+        private RecentCarParkFill recentCarParkFill;
+        public void Read() //could me modified for specific queires, then just retreive whole table
+        {
+            //Read DB table 
+            SqlCommand cmd = new SqlCommand(@"SELECT * FROM [dbo].[Car Park] ", sqlc);
+            DataTable Results = new DataTable();
+            // Read table from database and store it
+            sqlc.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Results.Load(reader);
+            int size = Results.Rows.Count;
+            sqlc.Close();
+
+            //Set database objects into a array, that can then be passed to webpage
+            recentCarParkFill = new RecentCarParkFill();
+            recentCarParkFill.Recent4 = new double[size];
+            int i = 0;
+            foreach (DataRow row in Results.Rows)
+            {
+                int cap = (int)row["Capacity"];
+                recentCarParkFill.Recent4[i] = (int)row["Occupied"] / ((double)cap) * 100.0;
+                i++;
+            }
+        }
+    }
+}
+
+public class RecentCarParkFill
+{
+    public double[] Recent4 { get; set; }
+}
